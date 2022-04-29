408c48e36e8a6c9ba877f8269d8e0797b3671bea
betrion <forteee@gmail.com>
2022-04-29 15:07:23 +0200

    dodan delete botun

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let imeSelect = document.getElementById("ime");
 let prezimeSelect = document.getElementById("prezime");
 let rezultat = document.getElementById("result");
+let brisiBotun = document
+  .querySelector("#delete")
+  .addEventListener("click", () => {
+    event.preventDefault();
+    let divovi = document.querySelectorAll("#fy");
+    console.log(divovi);
+    divovi.forEach((div) => div.remove());
+  });
 let botun = document.querySelector("#send").addEventListener("click", () => {
   event.preventDefault();
-  rezultat.innerText = `Fuck you ${imeSelect.value} ${prezimeSelect.value}`;
+  rezultat.innerHTML += `<div id="fy">Fuck you ${imeSelect.value} ${prezimeSelect.value}</div>`;
 });
